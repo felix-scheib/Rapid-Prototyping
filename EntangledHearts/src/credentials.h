02e9b698491262364fd0de995f8cb696ba4b3009
Felix Scheib <felix.scheib@stud.h-da.de>
2023-11-28 10:57:36 +0000

    ADD: added credentials.h template

---
+++ EntangledHearts/src/credentials.h
@@ -0,0 +1,7 @@
+#define WIFI_SSID "Your WiFi-SSID"
+#define WIFI_PASS "Your WiFi-Password"
+
+#define MQTT_BROKER "Your MQTT-Broker"
+#define MQTT_PORT 1883
+#define MQTT_USER "Your MQTT-User"
+#define MQTT_PASS "Your MQTT-Password"
