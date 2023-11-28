88c0a7da09f34e760710e83b132bb1da1da19d6d
Felix Scheib <felix.scheib@stud.h-da.de>
2023-11-28 11:00:28 +0000

    DEL: credentials.h deleted

--- EntangledHearts/src/credentials.h
+++
@@ -1,7 +0,0 @@
-#define WIFI_SSID "Your WiFi-SSID"
-#define WIFI_PASS "Your WiFi-Password"
-
-#define MQTT_BROKER "Your MQTT-Broker"
-#define MQTT_PORT 1883
-#define MQTT_USER "Your MQTT-User"
-#define MQTT_PASS "Your MQTT-Password"
